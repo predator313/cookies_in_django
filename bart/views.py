efb2dc311b429f5dd98090af04d9899966a13c2f
Aamir Ashraf <aamirnasa97@gmail.com>
2023-01-30 20:38:59 +0530

    create getcookies function

--- bart/views.py
+++ bart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#function to set cookies
+def setcookies(request):
+    response=render(request,'bart/setcookies.html')
+    response.set_cookie('name','zaid')
+    return response;
